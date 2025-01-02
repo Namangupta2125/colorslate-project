b6d0cb230a7d5d41cda61f798f8943009999b47a
Naman Gupta <guptanamandream21sd@gmail.com>
2025-01-02 16:00:09 +0530

    add clipboard functionality

--- src/Components/Colorpalet.jsx
+++ src/Components/Colorpalet.jsx
@@ -1,6 +1,18 @@
+import {toast} from 'react-toastify'
 function Colorpalet({percentage,code,fontcolor}) {
+  
+  const handleclick = async ()=>{
+    try{
+        await navigator.clipboard.writeText( '#'+code);
+        toast.success('copied to clipboard')
+    }
+    catch(error)
+    {
+      toast.error(error)
+    }
+  }
   return (
-    <div style={{display:'flex',flexDirection:"column",justifyContent:"start", alignItems:"start",backgroundColor:"#"+code,padding:"15px",height:"160px",width:"192px",color:fontcolor,boxSizing:"border-box",fontWeight:"30%"}}>{percentage} % <br /> {"#"+code}</div>
+    <div  onClick={handleclick} style={{display:'flex',flexDirection:"column",justifyContent:"start", alignItems:"start",backgroundColor:"#"+code,padding:"15px",height:"160px",width:"192px",color:fontcolor,boxSizing:"border-box",fontWeight:"30%"}}>{percentage} % <br /> {"#"+code}</div>
   )
 }
 export default Colorpalet
